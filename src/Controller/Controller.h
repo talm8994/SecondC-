78ca39cbd0b1f31bdeaeb4eed83a8cf4834f1a8a
talm8994 <tom.tom.pop13@gmail.com>
2016-01-19 12:42:18 -0700

    contoller

---
+++ src/Controller/Controller.h
@@ -0,0 +1,24 @@
+/*
+ * Controller.h
+ *
+ *  Created on: Jan 19, 2016
+ *      Author: talm8994
+ */
+
+#ifndef CONTROLLER_CONTROLLER_H_
+#define CONTROLLER_CONTROLLER_H_
+
+class SecondController
+{
+private:
+	int classCount;
+public:
+	SecondController();
+	void seetClassCount (int classCount);
+	int getClassCount();
+	void start();
+};
+
+
+
+#endif /* CONTROLLER_CONTROLLER_H_ */
